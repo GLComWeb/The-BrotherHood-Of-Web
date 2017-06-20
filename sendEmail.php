255b182f0eecf58990610f69f4522a2156457247
txwillys <txwillys@gmail.com>
2017-06-20 16:39:09 +0200

    modification fichier sendEmail

--- sendEmail.php
+++ sendEmail.php
@@ -1,29 +1,38 @@
  <?php
 
+// Constante de longueur de chaîne
+define('LEN_NAME', 2);		// longueur du nom
+define('LEN_MESSAGE', 15);  // longueur du message
+
 // Replace this with your own email address
 $siteOwnersEmail = 'gerard.lerandy@gmail.com';
 $message = '';
 
-if($_POST) {
+if ( isset($_POST['name'], $_POST['email'], $_POST['subject'], $_POST['message'] ) ) {
 	//Tu peux modifier ça si tu veux Depuis glcomweb.com - 
-	$subject = "Depuis glcomweb.com";  
+	//$subject = "Depuis glcomweb.com";  
 	
-   $name = trim(stripslashes($_POST['name']));
-   $email = trim(stripslashes($_POST['email']));
-   $subject .= trim(stripslashes($_POST['subject']));
-   $contact_message = trim(stripslashes($_POST['message']));
+    $name 			 = trim(stripslashes($_POST['name']));
+
+	// Vérifie que l'adresse email est correct
+	$email 			 = filter_var( trim(stripslashes($_POST['email'])), FILTER_VALIDATE_EMAIL );
+
+    $subject 		 = trim(stripslashes($_POST['subject']));
+    $contact_message = trim(stripslashes($_POST['message']));
 
    // Check Name
-	if (strlen($name) < 2) {
-		$error['name'] = "Entrer votre nom, s'il vous plaît.";
+	if ( strlen($name) < LEN_NAME ) {
+		$error['name'] = "Votre nom doit contenir au moins 2 caractères!";
 	}
 	// Check Email
-	if (!preg_match('/^[a-z0-9&\'\.\-_\+]+@[a-z0-9\-]+\.([a-z0-9\-]+\.)*+[a-z]{2}/is', $email)) {
-		$error['email'] = "Entrer un email valide, s'il vous plaît.";
-	}
+	// if (!preg_match('/^[a-z0-9&\'\.\-_\+]+@[a-z0-9\-]+\.([a-z0-9\-]+\.)*+[a-z]{2}/is', $email)) {
+	// 	$error['email'] = "Entrer un email valide, s'il vous plaît.";
+	// }
+	
+
 	// Check Message
-	if (strlen($contact_message) < 15) {
-		$error['message'] = "Entrer votre message, s'il vous plaît. Il devrait avoir au moins 15 caractères.";
+	if ( strlen($contact_message) < LEN_MESSAGE ) {
+		$error['message'] = "Votre message doit contenir au moins 15 caractères";
 	}
    // Subject
 	//if ($subject == '') { $subject = "Contact Form Submission"; }
@@ -31,13 +40,17 @@ if($_POST) {
 
    // Set Message
    $message .= "Email from: " . $name . "<br />";
-	$message .= "Email address: " . $email . "<br />";
+   $message .= "Email address: " . $email . "<br />";
    $message .= "Message: <br />";
    $message .= $contact_message;
    $message .= "<br /> ----- <br /> Cette e-mail a été envoyé à partir du formulaire de contact de votre site. <br />";
 
+   if (isset($error['name']) ) {
+   	echo '[ERROR] - '.$error['name'];
+   }
+
    // Set From: header
-   $from =  $name . " <" . $email . ">";
+   $from = $name . ' < ' . $email . ' > ';
 
    // Email Headers
 	/*    
@@ -47,24 +60,23 @@ if($_POST) {
 	$headers est la variable qui contient l'entete du mail
 	*/
 	
-	$headers = "From: " . $email . "\r\n";
+	$headers  = "From: " . $email . "\r\n";
  	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-
-   if (empty($error)) {
+    if (empty($error)) {
 
       ini_set("sendmail_from", $siteOwnersEmail); // for windows server
       $mail = mail($siteOwnersEmail, $subject, $message, $headers);
 
-		if ($mail) { echo "OK"; }
-      else { echo "Quelque chose s'est mal passé. Veuillez réessayer."; }
+	  if ($mail) { echo "Email Envoyé!"; }
+      else { echo "Erreur dans l'envoi du mail"; }
 		
 	} # end if - no validation error
 
 	else {
 
-		$response = (isset($error['name'])) ? $error['name'] . "<br /> \n" : null;
+		$response  = (isset($error['name'])) ? $error['name'] . "<br /> \n" : null;
 		$response .= (isset($error['email'])) ? $error['email'] . "<br /> \n" : null;
 		$response .= (isset($error['message'])) ? $error['message'] . "<br />" : null;
 		
@@ -72,6 +84,8 @@ if($_POST) {
 
 	} # end if - there was a validation error
 
+} else {
+	echo '[ERROR] - Vous n\'avez pas remplis correctement vos champs';
 }
 
 ?>
