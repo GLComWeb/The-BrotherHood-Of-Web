f7267bfe0a1f02977f5624f858c7597ca7a7a2bc
LucileSl <l.staal.pro@gmail.com>
2017-06-29 17:10:52 +0200

    Modif du 29/06/2017

--- dashboard_student.php
+++ dashboard_student.php
@@ -25,80 +25,84 @@
 	<body>
 		<!-- HEADER -->
 		<header>
-			<!-- SIDEBAR -->
-			<ba-sidebar>
-				<aside class="al-sidebar">
-					<div>
-						<ul class="al-sidebar-list">
-							<li class="al-sidebar-list-item dashboard">
-								<a href="#"><i class="fa fa-home" aria-hidden="true"></i><span>Tableau de bord</span></a>
-							</li>
-							<li class="al-sidebar-list-item">
-								<a href="#"><i class="fa fa-user-circle-o" aria-hidden="true"></i><span>Profil</span></a>
-							</li>
-							<li class="al-sidebar-list-item">
-								<a href="#"><i class="fa fa-book" aria-hidden="true"></i><span>Modules</span></a>
-							</li>
-							<li class="al-sidebar-list-item">
-								<a href="#"><i class="fa fa-envelope" aria-hidden="true"></i><span>Messagerie</span></a>
-							</li>
-							<li class="al-sidebar-list-item">
-								<a href="#"><i class="fa fa-comment" aria-hidden="true"></i><span>Forum</span></a>
-							</li>
-							<li class="al-sidebar-list-item">
-								<a href="#"><i class="fa fa-bar-chart" aria-hidden="true"></i><span>Statistiques</span></a>
-							</li>
-							
-						</ul> <!-- ./..al-sidebar-list -->
-					</div>
-				</aside> <!-- ./..al-sidebar -->
-			</ba-sidebar>
-			
-			<!-- MENU TOP -->
-			<nav class="navbar navbar-fixed-top custom-navbar" role="navigation">
-				<div class="container-fluid">
-					<!-- Brand and toggle get grouped for better mobile display -->
-					<div class="navbar-header">
-						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-							<span class="sr-only">Toggle navigation</span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-						</button>
-						<a class="navbar-brand" href="index.html">Apolearn II</a>
-					</div>
-					<!-- Collect the nav links, forms, and other content for toggling -->
-					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-						<ul class="nav navbar-nav navbar-right">
-							<li><a href="#"><i class="fa fa-power-off" aria-hidden="true"></i><span>Déconnexion</span></a></li>
-						</ul>
-					</div> <!-- /.navbar-collapse -->
-				</div> <!-- /.container-fluid -->
-			</nav>
+			<!-- NAVIGATION -->
+			<div class="side-menu">
+			    <nav class="navbar navbar-default" role="navigation">
+			    <!-- Brand and toggle get grouped for better mobile display -->
+				    <div class="navbar-header">
+				        <div class="brand-wrapper">
+				            <!-- Hamburger -->
+				            <button type="button" class="navbar-toggle">
+				                <span class="sr-only">Toggle navigation</span>
+				                <span class="icon-bar"></span>
+				                <span class="icon-bar"></span>
+				                <span class="icon-bar"></span>
+				            </button>
+
+				            <!-- Brand -->
+				            <div class="brand-name-wrapper">
+				                <a class="navbar-brand" href="#">Apolearn II</a>
+				            </div>
+
+				            <!-- Search -->
+				            <a data-toggle="collapse" href="#search" class="btn btn-default" id="search-trigger"><i class="fa fa-search" aria-hidden="true"></i></a>
+
+				            <!-- Search body -->
+				            <div id="search" class="panel-collapse collapse">
+				                <div class="panel-body">
+				                    <form class="navbar-form" role="search">
+				                        <div class="form-group">
+				                            <input type="text" class="form-control" placeholder="Search">
+				                        </div>
+				                        <button type="submit" class="btn btn-default "><i class="fa fa-check" aria-hidden="true"></i></button>
+				                    </form>
+				                </div>
+				            </div>
+				        </div> <!-- ./..brand-wrapper -->
+				    </div> <!-- ./..navbar-header -->
+
+				    <!-- Main Menu -->
+				    <div class="side-menu-container">
+				        <ul class="nav navbar-nav">
+				            <li class="active"><a href="#"><i class="fa fa-home" aria-hidden="true"></i> Tableau de bord</a></li>
+				            <li><a href="#"><i class="fa fa-user-circle-o" aria-hidden="true"></i> Profil</a></li>
+				            <li><a href="#"><i class="fa fa-book" aria-hidden="true"></i> Modules</a></li>
+				            <li><a href="#"><i class="fa fa-envelope" aria-hidden="true"></i> Messagerie</a></li>
+				            <li><a href="#"><i class="fa fa-comment" aria-hidden="true"></i> Forum</a></li>
+				            <li><a href="#"><i class="fa fa-power-off" aria-hidden="true"></i> Déconnexion</a></li>
+				        </ul>
+				    </div><!-- ./..side-menu-container -->
+				</nav> <!-- ./..navbar navbar-default -->
+   		 	</div> <!-- ./..side-menu -->
 		</header>
 
 		<!-- CONTENT -->
-		<main class="container">
-			<!-- CALENDAR -->
+		<main class="container-fluid">
 			<div class="row">
-				<div class="col-xs-12 calendar">
-					<h2>Calendar</h2>
-				</div> <!-- ./..col-xs-12 -->
-			</div> <!-- ./..row -->
-			<!-- HOMEWORK -->
-			<div class="row">
-				<div class="col-xs-12 homework">
-					
-				</div> <!-- ./..col-xs-12 -->
-			</div> <!-- ./..row -->
-			<!-- FORUM & CHAT -->
-			<div class="row">
-				<div class="col-xs-12 col-sm-6 forum">
-					
-				</div> <!-- ./..col-xs-12 col-sm-6 -->
-				<div class="col-xs-12 col-sm-6 chat">
-					
-				</div> <!-- ./..col-xs-12 col-sm-6 -->
+				<!-- CONTENT -->
+				<div class="col-sm-10">
+					<!-- CALENDAR -->
+					<div class="row">
+						<div class="col-xs-12 calendar">
+							<!-- <h2>Calendar</h2> -->
+						</div> <!-- ./..col-xs-12 -->
+					</div> <!-- ./..row calendar -->
+					<!-- HOMEWORK -->
+					<div class="row">
+						<div class="col-xs-12 homework">
+							<!-- <h2>Modules de cours</h2> -->
+						</div> <!-- ./..col-xs-12 -->
+					</div> <!-- ./..row homework -->
+					<!-- FORUM & CHAT -->
+					<div class="row">
+						<div class="col-xs-12 col-sm-6 forum">
+							<!-- <h2>Forum</h2> -->
+						</div> <!-- ./..col-xs-12 col-sm-6 forum -->
+						<div class="col-xs-12 col-sm-6 chat">
+							<!-- <h2>Chat</h2> -->
+						</div> <!-- ./..col-xs-12 col-sm-6 chat -->
+					</div> <!-- ./..row -->
+				</div> <!-- ./..col-sm-10 -->
 			</div> <!-- ./..row -->
 		</main> <!-- ./..container -->
 
